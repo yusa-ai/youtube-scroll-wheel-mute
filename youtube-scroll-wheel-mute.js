11d169ae1b5b17a327b037c737e19de80f966d5c
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-02-27 01:07:04 +0100

    Update youtube-scroll-wheel-mute.js

--- youtube-scroll-wheel-mute.js
+++ youtube-scroll-wheel-mute.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name        YouTube Scroll Wheel Mute
-// @namespace   Ryan Malonzo
+// @namespace   https://github.com/yusa-ai
 // @match       *://www.youtube.com/*
 // @grant       none
 // @version     1.0
 // @author      yusa-ai
+// @license     MIT
 // @description Toggles player mute state on mouse middle (scroll wheel) click
-// @require https://gitcdn.link/repo/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
+// @require     https://gitcdn.link/repo/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
+// @updateURL   https://raw.githubusercontent.com/yusa-ai/youtube-scroll-wheel-mute/main/youtube-scroll-wheel-mute.js
+// @downloadURL https://raw.githubusercontent.com/yusa-ai/youtube-scroll-wheel-mute/main/youtube-scroll-wheel-mute.js
+// @supportURL  https://raw.githubusercontent.com/yusa-ai/youtube-scroll-wheel-mute/issues
 // ==/UserScript==
 
 (() => {
